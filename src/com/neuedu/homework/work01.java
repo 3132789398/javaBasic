c9f2479550cb59c8789e9e5d13b159d8e4a4c223
3132789398 <3132789398@qq.com>
2019-05-23 19:20:58 +0800

    ghjgh

---
+++ src/com/neuedu/homework/work01.java
@@ -0,0 +1,15 @@
+package com.neuedu.homework;
+
+public class work01 {
+    public static void main(String[] args) {
+        int a=815;
+        int b=a%10;
+        int c=(a%100-b)/10;
+        int d=(a%1000-b-c*10)/100;
+
+        System.out.print(b);
+        System.out.print(c);
+        System.out.print(d);
+
+    }
+}
