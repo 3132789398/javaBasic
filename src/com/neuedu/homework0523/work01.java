0cd249f5994989d8f5f77ca9262d9fc5d86ea025
3132789398 <3132789398@qq.com>
2019-05-25 17:07:17 +0800

    作业

--- src/com/neuedu/homework0523/work01.java
+++ src/com/neuedu/homework0523/work01.java
@@ -1,4 +1,4 @@
-package com.neuedu.homework;
+package com.neuedu.homework0523;
 
 public class work01 {
     public static void main(String[] args) {
@@ -6,10 +6,8 @@ public class work01 {
         int b=a%10;
         int c=(a%100-b)/10;
         int d=(a%1000-b-c*10)/100;
-
         System.out.print(b);
         System.out.print(c);
         System.out.print(d);
-
     }
 }
