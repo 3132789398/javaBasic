59e88c395e78aa2edf5ab8ccf094f20f45321c4c
3132789398 <3132789398@qq.com>
2019-05-25 17:21:10 +0800

    作业

---
+++ src/com/neuedu/homework0524/work04.java
@@ -0,0 +1,12 @@
+package com.neuedu.homework0524;
+
+public class work04 {
+    public static void main(String[] args) {
+        // 计算9！
+        int a =1;
+        for(int i=1;i<=9;i++){
+            a=a*i;
+        }
+        System.out.println("9的阶乘结果为"+a);
+    }
+}
---
+++ src/com/neuedu/homework0524/work10.java
@@ -0,0 +1,24 @@
+package com.neuedu.homework0524;
+
+import java.util.Scanner;
+
+public class work10 {
+    public static void main(String[] args) {
+        //一个整数的各位数字之和能被 9 整除，则该数也能被 9 整除。编程验证给定的整数能否被 9 整除。
+        System.out.println("请输入一个数字：");
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b=0;
+        int e=a;
+        while (a%9!=0) {
+            b = b + (a % 10);
+            a = a /10;
+        }
+        if(b%9==0){
+            System.out.println(e+":这个数能被9整除");
+        }else {
+            System.out.println(e+":这个数不能被9整除");
+        }
+    }
+
+}
