0c126c341fcc1d875904fc1934cbb02cfc7e1a6e
3132789398 <3132789398@qq.com>
2019-05-28 19:16:42 +0800

    0527作业

--- src/com/neuedu/homework0527/test05.java
+++ src/com/neuedu/homework0527/test05.java
@@ -2,7 +2,19 @@ package com.neuedu.homework0527;
 
 public class test05 {
     public static void main(String[] args) {
-        work05 stu=new work05();
-        stu.student();
+           work05[] stus =new work05[5];
+           for(int i=0;i<stus.length;i++){
+               work05 work=new work05();
+               work.setName("abc");
+               work.setAges(16);
+               work.setFenshu(60+i);
+               stus[i] =work;
+
+           }
+        for(work05 s:stus){
+            System.out.println(s);
+        }
+
+
     }
 }
--- src/com/neuedu/homework0527/work05.java
+++ src/com/neuedu/homework0527/work05.java
@@ -3,10 +3,34 @@ package com.neuedu.homework0527;
 public class work05 {
 //定义一个学生类 包括 姓名 年龄 分数的特征
 //创建五个学生对象 对其中特征进行赋值 赋值后 根据分数从大到小进行排序
-    public void student(){
-        String name;
-        int b;
-        int c;
-        System.out.println("");
+     private String name;
+     private int ages;
+     private int fenshu;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAges() {
+        return ages;
+    }
+
+    public int getFenshu() {
+        return fenshu;
+    }
+
+    public void setName(String name1) {
+        name = name1;
+    }
+
+    public void setAges(int ages1) {
+        ages = ages1;
+    }
+
+    public void setFenshu(int fenshu1) {
+        fenshu = fenshu1;
+    }
+    public String toString(){
+               return "姓名"+getName()+"年龄"+ages+"成绩"+fenshu;
     }
 }
